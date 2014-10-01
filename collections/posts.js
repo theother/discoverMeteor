a28fee706a1e9e28a79b18ebe0de4aed7ba2982f
Tyler <tschultztt@gmail.com>
2014-10-01 13:37:17 -0500

    Removed the insecure package, thus had to make an allow function, to allow logged in users the ablity to submit posts

--- collections/posts.js
+++ collections/posts.js
@@ -1,4 +1,19 @@
+// Code here will be linted with JSHint.
+/* jshint ignore:start */
 
 //Global var - so no strict
 Posts = new Meteor.Collection('posts');
 
+// If the user is not logged in it will denie access
+// to submit posts to the DB
+Posts.allow({
+  insert: function (userId, doc) {
+    return !! userId;
+  }
+});
+
+
+
+
+// Code here will be linted with ignored by JSHint.
+/* jshint ignore:end */
