8a939800b784077c40f6a05ca838e1b45e839318
Tyler <tschultztt@gmail.com>
2014-10-01 15:30:44 -0500

    Created a function that calls a serverside method which ensures the post gets the green light and then returns the id of the newley reated post so the router knows where to navigate to.

--- client/views/posts/js/post_submit.js
+++ client/views/posts/js/post_submit.js
@@ -5,13 +5,13 @@ Template.postSubmit.events({
     e.preventDefault();
     // Collect the entered data from the form and put it in
     // a post object
-    var post ={
+    var thePost ={
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val(),
       message: $(e.target).find('[name=message]').val()
     };
 
-    Meteor.call('post', post, function (error, id) {
+    Meteor.call('post', thePost, function (error, id) {
       if(error){
         return alert(error.reason);
       }
--- collections/posts.js
+++ collections/posts.js
@@ -4,6 +4,42 @@
 //Global var - so no strict
 Posts = new Meteor.Collection('posts');
 
+Meteor.methods({
+  post: function (postAttributes) {
+    // Define a user variable
+    var user = Meteor.user();
+      // Since we pasted thePost into the call, we are able to find
+      // the attribute of the post object
+      postWithTheSameLink = Posts.findOne({url: postAttributes.url});
 
+    //ensure the user is logged in
+    if(!user)
+      throw new Meteor.Error(401, 'You need to login to post new stories');
+
+    //ensure the post has a title
+    if(!postAttributes.title)
+      throw new Meteor.Error(422, 'Add a title brother');
+
+    //Check that there are no previous post with the same link
+    if (postAttributes.url && postWithTheSameLink) {
+      throw new Meteor.Error(302,
+        'This link has already been posted',
+        postWithTheSameLink._id);
+    }
+
+    // pick out the whitelisted keys
+    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date().getTime()
+    });
+
+    // Insert the post into the DB
+    var postId = Posts.insert(post)
+
+    // Return _Id of the post for the route template
+    return postId;
+  }
+});
 // Code here will be linted with ignored by JSHint.
 /* jshint ignore:end */
